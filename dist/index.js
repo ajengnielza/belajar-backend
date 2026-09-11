5ae86aeb864d21a7328b9b0491f21451581721d6
ajengnielza <ajengnilza@gmail.com>
2026-09-11 14:14:41 +0700

    feat: complete week 9 - node.js with typescript

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-console.log("Server siap, ajeng! ");
-console.log("Node version:", process.version);
-console.log("Folder:", __dirname);
-console.log("Belajar Node.js Minggu 9!");
-//# sourceMappingURL=index.js.map
